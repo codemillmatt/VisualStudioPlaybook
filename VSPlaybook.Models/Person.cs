88b863541930478499aad0a19b644f104d1a3365
codemillmatt <msoucoup@codemilltech.com>
2022-05-08 15:16:32 -0700

    Initial commit

---
+++ VSPlaybook.Models/Person.cs
@@ -0,0 +1,22 @@
+﻿namespace VSPlaybook.Models
+{
+    public class Person
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+    }
+
+    public class Address
+    {
+        int _addressId;
+
+        public string? Line1 { get; set; }
+        public string? City { get; set; }
+        public string? State { get; set; }
+
+        public void AddNewAddress(string line, string city, string state)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
