fa657490409008b76ca42973553f213ef5462e84
codemillmatt <msoucoup@codemilltech.com>
2022-05-09 06:56:58 -0700

    Prepping for start of demo

---
+++ VSPlaybook.API/Controllers/PersonController.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace VSPlaybook.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonController : ControllerBase
+    { 
+        
+        private readonly ILogger<PersonController> _logger;
+
+        public PersonController(ILogger<PersonController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetPeople")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55)
+            })
+            .ToArray();
+        }
+    }
+}
