11d492d0d0173f4216080ec31263a30d6ce9e699
codemillmatt <msoucoup@codemilltech.com>
2022-05-09 18:26:34 -0700

    Initial check-in

---
+++ VisualStudioPlaybookLibrary/MyCoolClass.cs
@@ -0,0 +1,17 @@
+﻿namespace VisualStudioPlaybookLibrary
+{
+    public class MyCoolClass
+    {
+        public int IdentificationNumber { get; set; }
+
+        public void ThisIsNotAGoodFunctionName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void BuyCoffee()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
