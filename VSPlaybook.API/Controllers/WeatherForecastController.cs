f6a6574a868225824ef5f058a00382f6bd96175f
codemillmatt <msoucoup@codemilltech.com>
2022-05-27 18:29:34 -0700

    Getting ready for debug

--- VSPlaybook.API/Controllers/WeatherForecastController.cs
+++ VSPlaybook.API/Controllers/WeatherForecastController.cs
@@ -19,15 +19,22 @@ namespace VSPlaybook.API.Controllers
         }
 
         [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
+        public async Task<IEnumerable<WeatherForecast>> Get()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            await Task.Delay(2000);
+
+            var forecasts = Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = Random.Shared.Next(-20, 55),
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+            }).ToList();
+
+            WeatherForecast anotherOne = new() { Date = DateTime.Now, TemperatureC = 20, Summary = "Warm" };
+
+            forecasts.Add(anotherOne);
+
+            return forecasts;
         }
     }
 }
