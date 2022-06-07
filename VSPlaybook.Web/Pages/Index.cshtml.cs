7e67ac31bdc7355be81f3399f044cc35efc144c7
codemillmatt <msoucoup@codemilltech.com>
2022-06-07 13:11:04 -0700

    Updating for hot reload stuff

--- VSPlaybook.Web/Pages/Index.cshtml.cs
+++ VSPlaybook.Web/Pages/Index.cshtml.cs
@@ -6,10 +6,19 @@ namespace VSPlaybook.Web.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private string theInfo;
+        private string moreInfo;
+
+        public string TheInfo { get => theInfo; set => theInfo = value; }
+        public string MoreInfo { get => moreInfo; set => moreInfo = value; }
 
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
+
+            TheInfo = "This is the original info";
+
+            MoreInfo = "On file save makes it even better!";
         }
 
         public void OnGet()
